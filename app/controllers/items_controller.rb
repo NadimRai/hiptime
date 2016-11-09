909dda7f36d63cc234a5899d7b11dd934fc488ba
NadimRai <rai_nadim@yahoo.com>
2016-11-09 01:04:57 -0800

    Add completed_at to items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-	before_action :find_item, only:[:show, :edit, :update, :destroy]
+	before_action :find_item, only:[:show, :edit, :update, :destroy, :complete]
 	def index
 		if user_signed_in?
 			@items = Item.where(:user_id => current_user.id).order("created_at DESC")
@@ -37,6 +37,10 @@ class ItemsController < ApplicationController
 		@item.destroy
 		redirect_to root_path
 	end
+
+	def complete
+		@item.update_attribute(:completed_at, Time.now)
+		redirect_to root_path
 
 	private
 
